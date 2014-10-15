db5789b622897e2840da8858fe5cac952ac9f146
thejp <jp_55555@hotmail.com>
2014-10-15 11:09:38 +0200

    Changed search and remove

--- 03_AufgabeBinaryTree/src/ch/fhnw/algd2/a03/BinaryTree.java
+++ 03_AufgabeBinaryTree/src/ch/fhnw/algd2/a03/BinaryTree.java
@@ -6,15 +6,27 @@ public class BinaryTree {
 		if(root != null){ root.show(); }
 		System.out.println();
 	}
-	public Node search(int key){
+//	public Node search(int key){
+//		if(root != null){ return root.search(key); }
+//		return null;
+//	}
+	public boolean search(int key){
 		if(root != null){ return root.search(key); }
-		return null;
+		return false;
 	}
 	public void insert(int key){
 		if(root != null){ root.insert(key); }
 		else { root = new Node(key); }
 	}
-	public void remove(int key){
-		if(root != null){ root.remove(key); }
+//	public boolean remove(int key){
+//		if(root != null){ return root.remove(key); }
+//		return false;
+//	}
+	public boolean remove(int key){
+		Node capsule = new Node(key + 1);
+		capsule.left = root;
+		boolean result = root.remove(key, capsule, true);
+		root = capsule.left;
+		return result;
 	}
 }
--- 03_AufgabeBinaryTree/src/ch/fhnw/algd2/a03/Node.java
+++ 03_AufgabeBinaryTree/src/ch/fhnw/algd2/a03/Node.java
@@ -1,9 +1,9 @@
 package ch.fhnw.algd2.a03;
 
 public class Node {
-	private Node left;
-	private Node right;
-	private int key;
+	Node left;
+	Node right;
+	int key;
 
 	public Node(int key){
 		this.key = key;
@@ -15,11 +15,20 @@ public class Node {
 		System.out.print(key + ", ");
 		if(right != null){ right.show(); }
 	}
-	Node search(int key){
-		if(this.key == key){ return this; }
-		Node r = left != null ? left.search(key) : null;
-		return r != null ? r :
-			(right != null ? right.search(key) : null);
+//	Node search(int key){
+//		if(this.key == key){ return this; }
+//		Node r = left != null ? left.search(key) : null;
+//		return r != null ? r :
+//			(right != null ? right.search(key) : null);
+//	}
+	boolean search(int key){
+		if(this.key == key){ return true; }
+		else if(key < this.key){
+			if(left != null){ left.search(key); }
+		} else {
+			if(right != null){ right.search(key); }
+		}
+		return false;
 	}
 	void insert(int key){
 		if(key < this.key){
@@ -33,27 +42,51 @@ public class Node {
 	}
 	//boolean return increases performance for average cases:
 	//Interrupt and no search of rhs tree if the element was found in the lhs
-	public boolean remove(int key) {
+//	boolean remove(int key){
+//		if(this.key == key){
+//			if(left != null){
+//				this.key = left.key;
+//				if(right != null){
+//					//simple but degenerative method
+//					Node endLeft = left;
+//					while(endLeft.right != null){ endLeft = endLeft.right; }
+//					endLeft.right = right;
+//				}
+//				this.left = left.left;
+//				this.right = left.right;
+//			} else if(right != null){
+//				this.key = right.key;
+//				this.left = right.left;
+//				this.right = right.right;
+//			}
+//			return true;
+//		} else {
+//			if(left != null){ if(left.remove(key)){ return true; } }
+//			if(right != null){ if(right.remove(key)){ return false; } }
+//		}
+//		return false;
+//	}
+	boolean remove(int key, Node parent, boolean isLeft){
 		if(this.key == key){
 			if(left != null){
-				this.key = left.key;
-				if(right != null){
-					//simple but degenerative method
-					Node endLeft = left;
-					while(endLeft.right != null){ endLeft = endLeft.right; }
-					endLeft.right = right;
-				}
-				this.left = left.left;
-				this.right = left.right;
+				Node l = this;
+				while(l.left.left != null){ l = l.left; }
+				this.key = l.left.key;
+				//l.left.key = key; If it was not deleted
+				l.left = null;
 			} else if(right != null){
 				this.key = right.key;
 				this.left = right.left;
 				this.right = right.right;
+			} else {
+				if(isLeft){ parent.left = null; }
+				else { parent.right = null; }
 			}
 			return true;
+		} else if(key < this.key){
+			if(left != null){ return left.remove(key, this, true); }
 		} else {
-			if(left != null){ if(left.remove(key)){ return true; } }
-			if(right != null){ if(right.remove(key)){ return false; } }
+			if(right != null){ return right.remove(key, this, false); }
 		}
 		return false;
 	}
